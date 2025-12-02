e1932bb88db86f812f8edd52d122f617e3b367b3
Hasanga Ranasinghe <dinithhasangare@gmail.com>
2025-12-02 06:22:30 +0530

    Add method to check existence of subdomain in ProjectRepository

--- cloud-core/src/main/java/com/cloud/core/repository/ProjectRepository.java
+++ cloud-core/src/main/java/com/cloud/core/repository/ProjectRepository.java
@@ -4,4 +4,5 @@ import com.cloud.core.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    boolean existsBySubdomain(String subdomain);
 }
